84417e1901162ecfea0b72e26feee7c6e7d0ec88
YEOJU-SEOK <yeoju.ac@gmail.com>
2021-10-25 23:07:55 +0900

    boostcourse

---
+++ boostcourse/int.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <cs50.h>
+
+float average(int length, int array[]);
+
+int main(void)
+{
+    int n = get_int("numvers of scores: ");
+    int scores[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        scores[i] = get_int("Score %i: ", i+1);
+    }
+
+    printf("Average %.1f\n", average(n, scores));
+}
+
+float average(int length, int array[])
+{
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        sum += array[i];
+    }
+    return  (float) sum / (float) length;
+}
