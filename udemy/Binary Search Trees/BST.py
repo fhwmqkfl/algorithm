2dc5d1f507c578f06007398a8f6993404875fc30
YEOJU-SEOK <yeoju.ac@gmail.com>
2021-11-08 22:12:15 +0900

    udemy

--- udemy/Binary Search Trees/BST.py
+++ udemy/Binary Search Trees/BST.py
@@ -54,21 +54,25 @@ class BinarySearchTree:
         #     return False
         temp = self.root
 
-        while temp is not None:
-            if value < value.left:
+        while (temp is not None):
+            if value < temp.value:
                 temp = temp.left
-            elif value < value.right:
+            elif value < temp.value:
                 temp = temp.right
             else:
                 return True
         return False
 
 my_tree = BinarySearchTree()
-my_tree.insert(2)
-my_tree.insert(1)
-my_tree.insert(3)
+my_tree.insert(47)
+my_tree.insert(21)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
 
-
-print(my_tree.root.value)            
-print(my_tree.root.left.value)        
-print(my_tree.root.right.value)        
+#true            
+print(my_tree.contains(27))       
+#false 
+print(my_tree.contains(17))        
